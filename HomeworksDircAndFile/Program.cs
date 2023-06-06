e233158bce822e29a747406451d889a045d1dd2f
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-06-07 00:14:00 +0500

    Added 1.Task1

--- HomeworksDircAndFile/Program.cs
+++ HomeworksDircAndFile/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "D:\\github darslar\\Lesson006\\.gitignore";
+
+
+            if (Directory.Exists(path))
+            {
+                File.Create("D:\\github darslar\\Lesson006\\.gitignore\\.gitignore.tex");
+
+                Console.WriteLine("[Lesson006] papkadagi [.gitignore] papkaga" +
+                    " [gitignore.tex] fayl muvaffaqiyatli qo'shildi");
+            }
+
+            else
+            {
+                Directory.CreateDirectory(path);
+
+                File.Create("D:\\github darslar\\Lesson006\\.gitignore\\.gitignore.tex");
+                Console.WriteLine("[Lesson006] papkaga [.gitignore] papkasi yaratildi" +
+                    " va unga [gitignore.tex] fayl muvaffaqiyatli qo'shildi");
+            }
         }
     }
 }
