06cf47ccabba1b4737898d9472941e1e1ccb612a
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-06-07 00:24:48 +0500

    Added 1.Task3

--- HomeworksDircAndFile/Program.cs
+++ HomeworksDircAndFile/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "D:\\github darslar\\Lesson006\\.gitignore\\";
+
+            var directoryInfo = new DirectoryInfo(path);
+
+            FileInfo[] files = directoryInfo.GetFiles("*.docx");
+
+            if (!directoryInfo.Exists || files.Length == 0)
+            {
+                Console.WriteLine("Bunday addresli papka mavjud emas.");
+                Console.WriteLine("Yoki bunday formatdagi fayl mavjud emas.");
+
+                File.Create(path + "\\dokument1.docx");
+                File.Create(path + "\\dokument2.docx");
+                File.Create(path + "\\dokument3.docx");
+
+                Console.WriteLine(" \\ .docx \\ formatdagi 3 ta fayl yaratildi. ");
+            }
+
+            foreach (FileInfo fileInfo in files)
+            {
+                Console.WriteLine($" -- {fileInfo.FullName} --");
+            }
         }
     }
 }
