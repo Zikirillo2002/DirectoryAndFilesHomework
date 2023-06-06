3ff5bdca83bb317a29d0bffb4c0cdb95fcaac814
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-06-07 01:23:08 +0500

    Added 2.Task1

--- HomeworksDircAndFile/Program.cs
+++ HomeworksDircAndFile/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(GetDirectoryContent("D:\\github darslar\\Lesson006\\.gitignore\\"));
+        }
+
+        public static string GetDirectoryContent(string path)
+        {
+            if (Directory.GetFiles(path).Length == 0 && Directory.GetDirectories(path).Length == 0)
+            {
+                return "Papka bo'sh";
+            }
+            else if (Directory.GetFiles(path).Length == 0)
+            {
+                return "Papkalr bor";
+            }
+            else if (Directory.GetDirectories(path).Length == 0)
+            {
+                return "Fayllar bor";
+            }
+            else
+            {
+                return "Papkalar va fayllar bor";
+            }
         }
     }
 }
