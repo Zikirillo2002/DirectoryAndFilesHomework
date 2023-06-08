525a454abe8c04f0371ba59b7e7c518560867634
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-06-08 15:18:36 +0500

    Changed code

--- HomeworksDircAndFile/Program.cs
+++ HomeworksDircAndFile/Program.cs
@@ -4,25 +4,13 @@
     {
         static void Main(string[] args)
         {
-            string path = "D:\\github darslar\\Lesson006\\.gitignore";
+            string path = Directory.GetCurrentDirectory();
 
+            DirectoryInfo parentDirectory = Directory.GetParent(path);
 
-            if (Directory.Exists(path))
-            {
-                File.Create("D:\\github darslar\\Lesson006\\.gitignore\\.gitignore.tex");
+            parentDirectory = parentDirectory.Parent.Parent.Parent;
 
-                Console.WriteLine("[Lesson006] papkadagi [.gitignore] papkaga" +
-                    " [gitignore.tex] fayl muvaffaqiyatli qo'shildi");
-            }
-
-            else
-            {
-                Directory.CreateDirectory(path);
-
-                File.Create("D:\\github darslar\\Lesson006\\.gitignore\\.gitignore.tex");
-                Console.WriteLine("[Lesson006] papkaga [.gitignore] papkasi yaratildi" +
-                    " va unga [gitignore.tex] fayl muvaffaqiyatli qo'shildi");
-            }
+            File.Create($"{parentDirectory}\\.gitignore");
         }
     }
 }
