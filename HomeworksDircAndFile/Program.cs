ed8d62deb585a9447accb0deaecc9dba52899a52
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-06-11 23:12:48 +0500

    added 2.task2

--- HomeworksDircAndFile/Program.cs
+++ HomeworksDircAndFile/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Fayl manzilini kiriting" +
+                "(Misol : D:\\papka nomi\\...\\ fayl nomi.txt) : ");
+
+            string path = Console.ReadLine();
+
+            TextSearchLine(path);
+        }
+
+        static void TextSearchLine(string path)
+        {
+
+            if(File.Exists(path))
+            {
+                Console.WriteLine($" Fayl ichidagi textni qatorlari soni :" +
+                    $" {File.ReadAllLines(path).Length}."); 
+            }
+
+            else
+            {
+                Console.WriteLine($"Bunday fayl [ {path} ] mavjud emas.");
+            }
         }
     }
 }
